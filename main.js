af384075f49a71f7e929310322ab3e4e5a8fec1d
Chizoba Obasi <40410289+chizkidd@users.noreply.github.com>
2025-12-05 12:25:09 -0500

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-// pygbag runtime - auto-generated equivalent
-pygbag.run({
-    canvas: document.getElementById('canvas'),
-    main: 'main.py',
-    files: ['main.py', 'assets/background-day.png', 'assets/bluebird-midflap.png', 'assets/pipe-green.png']
-});
